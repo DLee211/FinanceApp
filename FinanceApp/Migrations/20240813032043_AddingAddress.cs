fcb1e5388b4d328495d46540fb1eef8c456c86d5
DLee211 <90346764+DLee211@users.noreply.github.com>
2024-08-12 23:22:26 -0400

    Added Address, able to register onto Database

---
+++ FinanceApp/Migrations/20240813032043_AddingAddress.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FinanceApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+        }
+    }
+}
